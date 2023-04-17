e82dfb78bb84ce9461abc482b5a714443935362c
markseu <mark2011@mayberg.se>
2023-04-18 01:37:19 +0200

    Updated extension, value converter

--- youtube.php
+++ youtube.php
@@ -2,7 +2,7 @@
 // Youtube extension, https://github.com/annaesvensson/yellow-youtube
 
 class YellowYoutube {
-    const VERSION = "0.8.6";
+    const VERSION = "0.8.7";
     public $yellow;         // access to API
     
     // Handle initialisation
@@ -17,6 +17,9 @@ class YellowYoutube {
         if ($name=="youtube" && ($type=="block" || $type=="inline")) {
             list($id, $style, $width, $height) = $this->yellow->toolbox->getTextArguments($text);
             if (is_string_empty($style)) $style = $this->yellow->system->get("youtubeStyle");
+            if (is_string_empty($height)) $height = $width;
+            $width = $this->convertValueAndUnit($width, 640);
+            $height = $this->convertValueAndUnit($height, 360);
             $language = $page->get("language");
             $output = "<div class=\"".htmlspecialchars($style)."\">";
             $output .= "<iframe src=\"https://www.youtube.com/embed/".rawurlencode($id)."?hl=".rawurlencode($language)."\" frameborder=\"0\" allow=\"fullscreen\"";
@@ -24,5 +27,16 @@ class YellowYoutube {
             $output .= "></iframe></div>";
         }
         return $output;
+    }
+    
+    // Return value according to unit
+    public function convertValueAndUnit($text, $valueBase) {
+        $value = $unit = "";
+        if (preg_match("/([\d\.]+)(\S*)/", $text, $matches)) {
+            $value = $matches[1];
+            $unit = $matches[2];
+            if ($unit=="%") $value = $valueBase * $value / 100;
+        }
+        return intval($value);
     }
 }
